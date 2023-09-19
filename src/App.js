9f94158da188b3588e9d76ff2ac30dec4c7b27b4
ircamitoc <iraincamitoc@gmail.com>
2023-09-19 22:28:55 +0800

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [loadMore, setLoadMore] = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=151"
+  );
+
+  const getAllPokemons = async () => {
+    const res = await fetch(loadMore);
+    const data = res.json();
+
+    setLoadMore(data.next);
+  };
+
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
+
   return (
     <div className="app-container">
       <h1>Pokemon Evolution</h1>
